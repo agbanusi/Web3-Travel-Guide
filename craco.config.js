63b8f5af51c9c81a57644cd6593bc6465c9a2da5
John Agbanusi <agbanusijohn@gmail.com>
2023-05-16 09:02:36 +0100

    setup config

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,14 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
 module.exports = {
-    style: {
-      postcss: {
-        plugins: [
-          require('tailwindcss'),
-          require('autoprefixer'),
-        ],
-      },
+  style: {
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
     },
-  }
+  },
+  webpack: {
+    plugins: {
+      add: [new NodePolyfillPlugin()],
+    },
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  purge: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  mode: "jit",
   theme: {
     extend: {},
   },
@@ -8,4 +8,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
